02d934bbcaeb3f5c5a576d932d929e46e5b58f92
S.Eyvazi <s.eyvazi@middleeasebank.ir>
2024-08-05 12:27:07 +0330

    Fix apply signal :bug:

--- src/training.py
+++ src/training.py
@@ -2,13 +2,14 @@ import pandas as pd
 import plotly.graph_objects as go
 import yfinance as yf
 from plotly.subplots import make_subplots
-from utils.utility import path_exists
+from utils.utility import path_exists, apply_total_signal, pointpos
 from datetime import datetime
 import pandas_ta as ta
 
-tickers_list = ['USDJPY=X', 'USDCHF=X', 'GC=F', 'EURUSD=X']
+tickers_list = ['USDJPY=X', 'USDCHF=X', 'GC=F', 'EURUSD=X', 'BMW.DE']
+
 download_path = './downloads'
-start_time_for_gathering_data = '2014-01-01'
+start_time_for_gathering_data = '2011-01-01'
 end_time_for_gathering_data = datetime.today().strftime('%Y-%m-%d')
 interval_for_gathering_time = '1d'
 
@@ -30,68 +31,86 @@ def fetch_data():
     df = yf.download(tickers=tickers_list, start=start_time_for_gathering_data, end=end_time_for_gathering_data,
                      interval=interval_for_gathering_time, group_by='ticker')
 
-
     df_usdjpy_candle_1d_2000_01_01_to_2024_08_04 = df[tickers_list[0]]
     df_usdchf_candle_1d_2000_01_01_to_2024_08_04 = df[tickers_list[1]]
     df_gold_candle_1d_2000_01_01_to_2024_08_04 = df[tickers_list[2]]
     df_eurusd_candle_1d_2000_01_01_to_2024_08_04 = df[tickers_list[3]]
+    df_bmw_de_candle_1d_2000_01_01_to_2024_08_04 = df[tickers_list[4]]
 
     df_gold_candle_1d_2000_01_01_to_2024_08_04.to_csv(
         download_path + '/GC_candle_' + interval_for_gathering_time + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
         mode='w+')
 
     df_usdchf_candle_1d_2000_01_01_to_2024_08_04.to_csv(
-        download_path + '/USDCHF_candle_' + interval_for_gathering_time  + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
+        download_path + '/USDCHF_candle_' + interval_for_gathering_time + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
         mode='w+')
 
     df_eurusd_candle_1d_2000_01_01_to_2024_08_04.to_csv(
-        download_path + '/EURUSD_candle_' + interval_for_gathering_time  + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
+        download_path + '/EURUSD_candle_' + interval_for_gathering_time + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
         mode='w+')
 
     df_usdjpy_candle_1d_2000_01_01_to_2024_08_04.to_csv(
-        download_path + '/USDJPY_candle_' + interval_for_gathering_time  + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
+        download_path + '/USDJPY_candle_' + interval_for_gathering_time + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
+        mode='w+')
+
+    df_bmw_de_candle_1d_2000_01_01_to_2024_08_04.to_csv(
+        download_path + '/BMW.DE_candle_' + interval_for_gathering_time + start_time_for_gathering_data + '_TO_' + end_time_for_gathering_data + '.csv',
         mode='w+')
 
     return [{'EURUSD': df_eurusd_candle_1d_2000_01_01_to_2024_08_04},
             {'GC': df_gold_candle_1d_2000_01_01_to_2024_08_04},
             {'USDCHF': df_usdchf_candle_1d_2000_01_01_to_2024_08_04},
+            {'BMW.DE': df_bmw_de_candle_1d_2000_01_01_to_2024_08_04},
             {'USDJPY': df_usdjpy_candle_1d_2000_01_01_to_2024_08_04}], is_processed
 
 
 def calc_primary_indicator(df):
+    df = df[df.High != df.Low]
+
     df.ta.bbands(append=True, length=30, std=2)
     df.ta.rsi(append=True, length=14)
-    df["ATR"] = ta.atr(low=df.Low, close=df.Close, high=df.High, length=14)
+    df["atr"] = ta.atr(low=df.Low, close=df.Close, high=df.High, length=14)
 
-    # Rename columns for clarity if necessary
     df.rename(columns={
-        'BBL_30_2.0': 'BBL', 'BBM_30_2.0': 'BBM', 'BBU_30_2.0': 'BBH', 'RSI_14': 'RSI'
+        'BBL_30_2.0': 'bbl', 'BBM_30_2.0': 'bbm', 'BBU_30_2.0': 'bbh', 'RSI_14': 'rsi'
     }, inplace=True)
 
     # Calculate Bollinger Bands Width
-    df['BB_WIDTH'] = (df['BBH'] - df['BBL']) / df['BBM']
+    df['bb_width'] = (df['bbh'] - df['bbl']) / df['bbm']
+
+    df.rename(
+        columns={'bbl': 'BBL', 'bb_width': 'BB_WIDTH', 'bbm': 'BBM', 'bbh': 'BBH', 'rsi': 'RSI'},
+        inplace=True)
+
+    return df
 
 
 def plot_chart_with_indicators(df, label, st):
-    dfpl = df[st:st + 350]
+    dfpl = df
     fig = make_subplots(rows=2, cols=1)
-    fig.add_trace(go.Candlestick(x=dfpl.index,
+    fig.add_trace(go.Candlestick(x=dfpl.Date,
                                  open=dfpl['Open'],
                                  high=dfpl['High'],
                                  low=dfpl['Low'],
                                  close=dfpl['Close']),
                   row=1, col=1)
 
-    fig.add_trace(go.Scatter(x=dfpl.index, y=dfpl['BBL'],
+    fig.add_trace(go.Scatter(x=dfpl.Date, y=dfpl['BBL'],
                              line=dict(color='blue', width=1),
                              name="BBL"),
                   row=1, col=1)
-    fig.add_trace(go.Scatter(x=dfpl.index, y=dfpl['BBH'],
+    fig.add_trace(go.Scatter(x=dfpl.Date, y=dfpl['BBH'],
                              line=dict(color='green', width=1),
                              name="BBU"),
                   row=1, col=1)
 
-    fig.add_trace(go.Scatter(x=dfpl.index, y=dfpl['RSI'],
+    # Add markers for trade entry points on the same subplot
+    fig.add_trace(go.Scatter(x=dfpl.Date, y=dfpl['pointpos'], mode="markers",
+                             marker=dict(size=8, color="MediumPurple"),
+                             name="entry"),
+                  row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=dfpl.Date, y=dfpl['RSI'],
                              line=dict(color='green', width=2),
                              name="RSI"),
                   row=2, col=1)
@@ -109,12 +128,28 @@ for item in fetched_data_list:
     ticker_list.append(list(item.items())[0][0])
 
 if not is_processed:
-    for index in range(0, len(df_list)): calc_primary_indicator(df_list[index])
-    df_list[index].reset_index(inplace=True)
-    df_list[index].set_index('Date', inplace=True)
-    for index in range(0, len(df_list)): df_list[index].to_csv(download_path + "/" + ticker_list[index] + '.csv')
+    for index in range(0, len(df_list)):
+        df_list[index] = calc_primary_indicator(df_list[index])
+        df_list[index] = df_list[index].round(4)
+        df_list[index].reset_index(inplace=True)
+        df_list[index].set_index('Date', inplace=True)
+        df_list[index].to_csv(download_path + "/" + ticker_list[index] + '.csv')
 
+total_signal_length = 0
 for index in range(0, len(df_list)):
-    df_list[index].reset_index(inplace=True)
-    df_list[index].set_index('Date', inplace=True)
-    plot_chart_with_indicators(df_list[index], ticker_list[index], st=500)
+    df= df_list [index]
+    df.ta.bbands(append=True, length=30, std=2)
+    df.ta.rsi(append=True, length=14)
+    df["atr"] = ta.atr(low = df.Low, close = df.Close, high = df.High, length=14)
+    df.rename(columns={
+        'BBL_30_2.0': 'bbl', 'BBM_30_2.0': 'bbm', 'BBU_30_2.0': 'bbh', 'RSI_14': 'rsi'
+    }, inplace=True)
+
+    # Calculate Bollinger Bands Width
+    df['bb_width'] = (df['bbh'] - df['bbl']) / df['bbm']
+    apply_total_signal(df=df, rsi_threshold_low=30, rsi_threshold_high=70, bb_width_threshold=0.001)
+    df['pointpos'] = df.apply(lambda row: pointpos(row), axis=1)
+    total_signal_length += len(df[df['TotalSignal'] != 0] )
+    plot_chart_with_indicators(df, ticker_list[index], st=500)
+
+print("Total Signal Count Is: {i}", total_signal_length)
--- src/utils/utility.py
+++ src/utils/utility.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 
 def path_exists(directory, start, end):
@@ -10,3 +11,49 @@ def path_exists(directory, start, end):
         if file.startswith(start) and file.endswith(end):
             return directory + "/" + file
     return False
+
+
+def apply_total_signal(df, rsi_threshold_low=30, rsi_threshold_high=70, bb_width_threshold = 0.0015):
+    # Initialize the 'TotalSignal' column
+    df['TotalSignal'] = 0
+
+    for i in range(1, len(df)):
+        # Previous candle conditions
+        prev_candle_closes_below_bb = df['Close'].iloc[i-1] < df['bbl'].iloc[i-1]
+        prev_rsi_below_thr = df['rsi'].iloc[i-1] < rsi_threshold_low
+        # Current candle conditions
+        closes_above_prev_high = df['Close'].iloc[i] > df['High'].iloc[i-1]
+        bb_width_greater_threshold = df['bb_width'].iloc[i] > bb_width_threshold
+
+        # Combine conditions
+        if (prev_candle_closes_below_bb and
+                prev_rsi_below_thr and
+                closes_above_prev_high and
+                bb_width_greater_threshold):
+            df.at[i, 'TotalSignal'] = 2  # Set the buy signal for the current candle
+
+        # Previous candle conditions
+        prev_candle_closes_above_bb = df['Close'].iloc[i-1] > df['bbh'].iloc[i-1]
+        prev_rsi_above_thr = df['rsi'].iloc[i-1] > rsi_threshold_high
+        # Current candle conditions
+        closes_below_prev_low = df['Close'].iloc[i] < df['Low'].iloc[i-1]
+        bb_width_greater_threshold = df['bb_width'].iloc[i] > bb_width_threshold
+
+        # Combine conditions
+        if (prev_candle_closes_above_bb and
+                prev_rsi_above_thr and
+                closes_below_prev_low and
+                bb_width_greater_threshold):
+            df.at[i, 'TotalSignal'] = 1  # Set the sell signal for the current candle
+
+
+    return df
+
+
+def pointpos(x):
+    if x['TotalSignal'] == 2:
+        return x['Low'] - 1e-4
+    elif x['TotalSignal'] == 1:
+        return x['High'] + 1e-4
+    else:
+        return np.nan
